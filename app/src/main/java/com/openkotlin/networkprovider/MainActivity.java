fafca30ee86be354bb7fb5fcbd64a665fc88abec
Tanck <tanck.tang@pb.com>
2019-11-25 15:24:20 +0800

    First commit

---
+++ app/src/main/java/com/openkotlin/networkprovider/MainActivity.java
@@ -0,0 +1,30 @@
+package com.openkotlin.networkprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        assert wifiManager != null;
+        DhcpInfo wifiManagerDhcpInfo = wifiManager.getDhcpInfo();
+        if (wifiManagerDhcpInfo != null) {
+            String gateWay = intToIp(wifiManagerDhcpInfo.gateway);
+            Toast.makeText(this, "The gateway is : " + gateWay, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String intToIp(int paramInt) {
+        return (paramInt & 0xFF) + "." + (0xFF & paramInt >> 8) + "." + (0xFF & paramInt >> 16) + "."
+                + (0xFF & paramInt >> 24);
+    }
+}
